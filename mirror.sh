93967bc66bebf82d81c6a17e8a48cb4abec1605d
Jack Atkinson <jwa34@cam.ac.uk>
2023-12-23 14:13:22 +0000

    Add mirroring script.

---
+++ mirror.sh
@@ -0,0 +1,7 @@
+git clone --mirror git@gitlab.com:jatkinson1000/power-up-python.git
+
+cd power-up-python.git
+
+git remote add github git@github.com:jatkinson1000/power-up-python.git
+
+git push --mirror github
