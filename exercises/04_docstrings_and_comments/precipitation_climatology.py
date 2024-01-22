06f81fd60612109075f97554545a8b871e0aa074
Jack Atkinson <jwa34@cam.ac.uk>
2024-01-22 21:08:36 +0000

    Update ex 4 code.

--- exercises/04_docstrings_and_comments/precipitation_climatology.py
+++ exercises/04_docstrings_and_comments/precipitation_climatology.py
@@ -1,16 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 import xarray as xr
-import scipy
-import cf_xarray
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 import cmocean
 import regionmask
-
-
-import matplotlib.ticker as mticker
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
 
 def convert_pr_units(darray):
@@ -25,16 +21,6 @@ def convert_pr_units(darray):
     assert darray.data.max() < 2000, "There is a precipitation value/s > 2000 mm/day"
 
     return darray
-
-
-def apply_mask(darray, sftlf_file, realm):
-    # Function to mask ocean or land using a sftlf (land surface fraction) file.
-    # Inputs:
-    #  darray: Data to mask
-    #  sftlf_file: Land surface fraction file
-    #  realm: Realm to mask
-
-    pass
 
 
 def plot_zonal(data):
@@ -52,13 +38,13 @@ def plot_zonal(data):
     for axis in ax:
         axis.set_ylim(0.0, 1.0e-4)
         axis.grid()
-    plt.savefig("zonal.png", dpi=200)
+    plt.savefig("zonal.png", dpi=200)  # Save figure to file
 
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 5))
 
     zonal_pr.T.plot()
 
-    plt.savefig("zonal_map.png", dpi=200)
+    plt.savefig("zonal_map.png", dpi=200)  # Save figure to file
 
 
 def get_country_ann_avg(data, countries):
@@ -70,7 +56,7 @@ def get_country_ann_avg(data, countries):
     # List possible locations to plot
     # [print(k, v) for k, v in regionmask.defined_regions.natural_earth_v5_0_0.countries_110.regions.items()]
 
-    with open("data.txt", "w") as datafile:
+    with open("data.txt", "w", encoding="utf-8") as datafile:
         for k, v in countries.items():
             # land.plot(ax=geo_axes, add_label=False, fc="white", lw=2, alpha=0.5)
             # clim = clim.where(ocean == "South Pacific Ocean")
@@ -95,13 +81,14 @@ def get_country_ann_avg(data, countries):
             # gl.yformatter = LATITUDE_FORMATTER
             # gl.xlabel_style = {'size': 15, 'color': 'gray'}
             # gl.ylabel_style = {'size': 15, 'color': 'gray'}
-            # print(f"show {k}")
+            # print("show %s" %k)
             # plt.show()
 
             for yr in data_avg_mask.year.values:
-                # print(f"{k} has {data_avg_mask.sel(year = yr).mean().values} mm/day in {yr}")
                 precip = data_avg_mask.sel(year=yr).mean().values
-                datafile.write(f"{k.ljust(25)} {yr} : {precip:2.3f} mm/day\n")
+                datafile.write(
+                    "{} {} : {:2.3f} mm/day\n".format(k.ljust(25), yr, precip)
+                )
             datafile.write("\n")
 
 
@@ -125,20 +112,18 @@ def plot_enso(data):
     # plot:
     enso.plot()
 
-    plt.savefig("enso.png", dpi=200)
+    plt.savefig("enso.png", dpi=200)  # Save figure to file
 
 
 def create_plot(clim, model, season, mask=None, gridlines=False, levels=None):
     """Plot the precipitation climatology.
 
-    Args:
-      clim (xarray.DataArray): Precipitation climatology data
-      model (str): Name of the climate model
-      season (str): Season
+    clim (xarray.DataArray): Precipitation climatology data
+    model (str): Name of the climate model
+    season (str): Season
 
-    Kwargs:
-      gridlines (bool): Select whether to plot gridlines
-      levels (list): Tick marks on the colorbar
+    gridlines (bool): Select whether to plot gridlines
+    levels (list): Tick marks on the colorbar
 
     """
 
@@ -165,22 +150,26 @@ def create_plot(clim, model, season, mask=None, gridlines=False, levels=None):
         cmap=cmocean.cm.rain,
     )
 
-    geo_axes.add_feature(cfeature.COASTLINE, lw=2)
+    geo_axes.add_feature(
+        cfeature.COASTLINE, lw=2
+    )  # Add coastines using cartopy feature
 
     if mask:
-        # 2 if mask == "ocean":
-        # 2 mask_feat = cfeature.NaturalEarthFeature("physical", "ocean", "110m")
-        # 1 geo_axes.add_feature(cfeature.NaturalEarthFeature("physical", "ocean", "110m"),
-        #                          ec="red", fc="yellow", lw=2, alpha=1.0)
-        # 2 elif mask == "land":
-        # 2 mask_feat = cfeature.NaturalEarthFeature("physical", "land", "110m")
-        # 1 # geo_axes.add_feature(cfeature.NaturalEarthFeature("physical", "ocean", "110m"),
+        # Old approach of adding mask before combining into the below command.
+        # if mask == "ocean":
+        # old mask_feat = cfeature.NaturalEarthFeature("physical", "ocean", "110m")
+        # oldold geo_axes.add_feature(cfeature.NaturalEarthFeature("physical", "ocean", "110m"),
+        #                      ec="red", fc="yellow", lw=2, alpha=1.0)
+        # elif mask == "land":
+        # old mask_feat = cfeature.NaturalEarthFeature("physical", "land", "110m")
+        # oldold # geo_axes.add_feature(cfeature.NaturalEarthFeature("physical", "ocean", "110m"),
         #                           ec="red", fc="yellow", lw=2, alpha=1.0)
 
-        # 2 else:
-        # 2 pass
-        # 2 raise ValueError("Unknown ")
+        # oldold else:
+        # oldold pass
+        # oldold raise ValueError("Unknown ")
 
+        # Mask out (fade) using 110m resolution data from cartopy.
         geo_axes.add_feature(
             cfeature.NaturalEarthFeature("physical", mask, "110m"),
             ec=None,
@@ -188,9 +177,9 @@ def create_plot(clim, model, season, mask=None, gridlines=False, levels=None):
             lw=2,
             alpha=0.75,
         )
-        ###### WHAT IF WE PASS INVALID???
 
     if gridlines:
+        # If we want gridlines run the code to do this:
         gl = geo_axes.gridlines(
             crs=ccrs.PlateCarree(),
             draw_labels=True,
@@ -203,15 +192,17 @@ def create_plot(clim, model, season, mask=None, gridlines=False, levels=None):
         gl.left_labels = True
         # gl.xlines = False
         gl.xlocator = mticker.FixedLocator([-180, -90, 0, 90, 180])
-        gl.ylocator = mticker.FixedLocator([-66, -23, 0, 23, 66])
+        gl.ylocator = mticker.FixedLocator(
+            [-66, -23, 0, 23, 66]
+        )  # Tropics & Polar Circles
         gl.xformatter = LONGITUDE_FORMATTER
         gl.yformatter = LATITUDE_FORMATTER
         gl.xlabel_style = {"size": 15, "color": "gray"}
         gl.ylabel_style = {"size": 15, "color": "gray"}
 
-    title = f"{model} precipitation climatology ({season})"
+    title = "{} precipitation climatology ({})".format(model, season)
     plt.title(title)
-    # print(f"\n\n{clim.mean()}\n\n")
+    # print("\n\n{}\n\n".format(clim.mean()))
 
 
 def main(
@@ -221,9 +212,12 @@ def main(
     gridlines=False,
     mask=None,
     cbar_levels=None,
-    countries={"United Kingdom": "GB"},
+    countries=None,
 ):
     """Run the program."""
+
+    if countries is None:
+        countries = {"United Kingdom": "GB"}
 
     dset = xr.open_dataset(pr_file)
 
@@ -235,14 +229,13 @@ def main(
 
     try:
         input_units = clim.attrs["units"]
-    except KeyError:
+    except KeyError as exc:
         raise KeyError(
             "Precipitation variable in {pr_file} must have a units attribute"
-        )
+        ) from exc
 
     if input_units == "kg m-2 s-1":
         clim = convert_pr_units(clim)
-        # logging.info('Units converted from kg m-2 s-1 to mm/day')
     elif input_units == "mm/day":
         pass
     else:
@@ -257,21 +250,32 @@ def main(
         levels=cbar_levels,
     )
 
-    plt.savefig(output_file, dpi=200)
+    plt.savefig(output_file, dpi=200)  # Save figure to file
 
 
 if __name__ == "__main__":
-    input_file = "../../data/pr_Amon_ACCESS-ESM1-5_historical_r1i1p1f1_gn_201001-201412.nc"
-    season_to_plot = "JFM"
+    input_file = (
+        "../../data/pr_Amon_ACCESS-ESM1-5_historical_r1i1p1f1_gn_201001-201412.nc"
+    )
+    # season_to_plot = "DJF"
+    # season_to_plot = "MAM"
+    season_to_plot = "JJA"
+    # season_to_plot = "SON"
     output_filename = "output.png"
     gridlines_on = True
     mask_id = "ocean"
-    cbar_levels = None
-    countries = {
+    colorbar_levels = None
+    countries_to_record = {
         "United Kingdom": "GB",
         "United States of America": "US",
         "Antarctica": "AQ",
         "South Africa": "ZA",
     }
 
-    main(input_file, mask=mask_id, gridlines=gridlines_on, countries=countries)
+    main(
+        input_file,
+        season=season_to_plot,
+        mask=mask_id,
+        gridlines=gridlines_on,
+        countries=countries_to_record,
+    )
