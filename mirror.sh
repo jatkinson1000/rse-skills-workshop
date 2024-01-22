a583e4f8c4c170c2e5e6fc3ebbb3f8db210200e4
Jack Atkinson <jwa34@cam.ac.uk>
2024-01-22 16:12:18 +0000

    Update mirroring script.

--- mirror.sh
+++ mirror.sh
@@ -1,7 +1,10 @@
+#!usr/bin/env bash
+#
+# Script to set up mirroring from GitLab to GitHub. Requires ssh access to both.
+# Clone gitlab git file as a mirror, add a github remote, push to github as mirror.
+# Can be run from inside main repo.
+
 git clone --mirror git@gitlab.com:jatkinson1000/power-up-python.git
-
 cd power-up-python.git
-
 git remote add github git@github.com:jatkinson1000/power-up-python.git
-
 git push --mirror github
