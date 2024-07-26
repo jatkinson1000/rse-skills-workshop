ae27700f502a504b575e0836184799c192a2c7cc
Jack Atkinson <jwa34@cam.ac.uk>
2024-07-26 08:12:16 +0100

    Add option to mirror bi-directionally between gitlab and github.

---
+++ mirror_to_gitlab.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Script to set up mirroring from GitHub to GitLab. Requires ssh access to both.
+# Clone github git file as a mirror, add a gitlab remote, push to gitlab as mirror.
+# Can be run from inside main repo.
+
+rm -rf power-up-python.git
+git clone --mirror git@github.com:jatkinson1000/power-up-python.git
+cd power-up-python.git
+git remote add gitlab git@gitlab.com:jatkinson1000/power-up-python.git
+git push --mirror gitlab
