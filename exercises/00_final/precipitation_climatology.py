2c044243fd623cf8d94dd1881a91ec63f2bdaac1
Jack Atkinson <jwa34@cam.ac.uk>
2025-06-13 14:47:28 +0100

    Update slides and exercise code to combine magic numbers with naming and remove the 'better python' section for this version of the course.

--- exercises/00_final/precipitation_climatology.py
+++ exercises/00_final/precipitation_climatology.py
@@ -33,10 +33,14 @@ def convert_precipitation_units(precipitation_in_kg_per_m_squared_s):
 
     precipitation_in_mm_per_day.attrs["units"] = "mm/day"
 
+    precipitation_upper_bound = 2000  # mm/day
+
     if precipitation_in_mm_per_day.data.min() < 0.0:
         raise ValueError("There is at least one negative precipitation value")
-    if precipitation_in_mm_per_day.data.max() > 2000:
-        raise ValueError("There is a precipitation value/s > 2000 mm/day")
+    if precipitation_in_mm_per_day.data.max() > precipitation_upper_bound:
+        raise ValueError(
+            f"There are precipitation values > {precipitation_upper_bound} mm/day"
+        )
 
     return precipitation_in_mm_per_day
 
@@ -47,7 +51,7 @@ def plot_zonally_averaged_precipitation(precipitation_data):
 
     Parameters
     ----------
-    precipitation_data : xarray.DataSet
+    precipitation_data : xarray.DataArray
         xarray DataSet containing precipitation model data, specifying precipitation in
         [kg m-2 s-1] at given latitudes, longitudes and time. The Dataset should contain
         four aligned DataArrays: precipitation, latitude, longitude and time.
@@ -85,7 +89,7 @@ def get_country_annual_average(precipitation_data, countries):
 
     Parameters
     ----------
-    precipitation_data : xarray.DataSet
+    precipitation_data : xarray.DataArray
         xarray DataSet containing precipitation model data, specifying precipitation in
         [kg m-2 s-1] at given latitudes, longitudes and time. The Dataset should contain
         four aligned DataArrays: precipitation, latitude, longitude and time.
@@ -133,7 +137,7 @@ def plot_enso_hovmoller_diagram(precipitation_data):
 
     Parameters
     ----------
-    precipitation_data : xarray.DataSet
+    precipitation_data : xarray.DataArray
        xarray DataSet containing precipitation model data, specifying precipitation in
         [kg m-2 s-1] at given latitudes, longitudes and time. The Dataset should contain
         four aligned DataArrays: precipitation, latitude, longitude and time.
